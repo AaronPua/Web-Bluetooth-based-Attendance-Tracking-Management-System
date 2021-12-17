0ae521d012eec8bdb6635e09bd24e8df27bad4e1
Aaron Pua <aaronpua@hotmail.com>
2021-12-16 18:39:16 -0800

    Setup Elastic UI at the app root.

--- client/main.tsx
+++ client/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { App } from '/imports/ui/App'
+import { App } from '/imports/ui/App';
+import '@elastic/eui/dist/eui_theme_light.css';
+import { EuiProvider } from '@elastic/eui';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+  render(
+    <EuiProvider colorMode="light">
+      <App />
+    </EuiProvider>
+    , document.getElementById('react-target'));
 });
