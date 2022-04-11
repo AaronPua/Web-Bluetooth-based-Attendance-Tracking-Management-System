411bd5d89c6873ca10dc9052f4650a7cb6bacf9e
Aaron Pua <aaronpua@hotmail.com>
2022-04-11 13:51:12 -0700

    Students can login on mobile but not web app.

--- imports/server/Login.ts
+++ imports/server/Login.ts
@@ -1,14 +1,9 @@
 import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
-import { Roles } from 'meteor/alanning:roles'
 
 Accounts.validateLoginAttempt((options: any) => {
     if (!options.allowed) {
         return false;
-    }
-
-    if(Roles.userIsInRole(options.user, 'student')) {
-        throw new Meteor.Error('student-not-allowed', 'You do not have access to login');
     }
 
     if (options.user.emails[0].verified === true) {
--- imports/startup/client/routes/RequireAuth.tsx
+++ imports/startup/client/routes/RequireAuth.tsx
@@ -3,9 +3,14 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Navigate } from 'react-router-dom';
 
-export default function RequireAuth({children}: { children: JSX.Element }) {
+export default function RequireAuth({ children }: { children: JSX.Element }) {
 
-    const user = useTracker(() => Meteor.userId());
+    const userId = useTracker(() => Meteor.userId());
+
+    if(Roles.userIsInRole(userId, 'student')) {
+        Meteor.logout();
+        return <Navigate to='/' state={{ accessDenied: 'You do not have proper access to login'}} />;
+    }
     
-    return user ? children : <Navigate to='/' />;
+    return userId ? children : <Navigate to='/' state={{ loginFirst: 'You need to log in first'}} />;
 }
