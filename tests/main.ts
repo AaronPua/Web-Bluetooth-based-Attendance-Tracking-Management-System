f3f7a62610140a7100ea1ecfbd0ba4c08f8b89fc
Aaron Pua <aaronpua@hotmail.com>
2022-04-24 18:00:23 -0700

    Uninstall React-Testing-Library.

--- tests/main.ts
+++ tests/main.ts
@@ -1,16 +1,11 @@
-/* // Users
 import '../imports/api/users/tests/UsersMethods.test.ts';
 import '../imports/api/users/tests/UsersPublications.test.ts';
 
-// Courses
 import '../imports/api/courses/tests/CoursesMethods.test.ts';
 import '../imports/api/courses/tests/CoursesPublications.test.ts';
 
-// Lessons
 import '../imports/api/lessons/tests/LessonsMethods.test.ts';
-import '../imports/api/lessons/tests/LessonsPublications.test.ts'; */
+import '../imports/api/lessons/tests/LessonsPublications.test.ts';
 
-// Beacons
-import '../imports/ui/tests/beacons/Beacons.test.tsx';
-// import '../imports/api/beacons/tests/BeaconsMethods.test.ts';
-// import '../imports/api/beacons/tests/BeaconsPublications.test.ts';
+import '../imports/api/beacons/tests/BeaconsMethods.test.ts';
+import '../imports/api/beacons/tests/BeaconsPublications.test.ts';
