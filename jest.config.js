946e2ad503479a4921c87a421d194ae9eadd5ba4
Aaron Pua <aaronpua@hotmail.com>
2022-04-28 16:01:49 -0700

    Change environment to jest-environment-jsdom.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
+    testEnvironment: 'jest-environment-jsdom',
     globals: {
         "ts-jest": {
             tsconfig: "tsconfig.test.json",
