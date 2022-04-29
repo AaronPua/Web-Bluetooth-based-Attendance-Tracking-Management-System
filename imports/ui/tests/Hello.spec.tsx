3b743e804f7c33395d4afe4cf97c5ec1a5903962
Aaron Pua <aaronpua@hotmail.com>
2022-04-28 18:40:06 -0700

    Jest-dom typescript.

--- imports/ui/tests/Hello.spec.tsx
+++ imports/ui/tests/Hello.spec.tsx
@@ -5,9 +5,9 @@
 // import { expect } from 'chai';
 // import { renderWithRouter } from './setupTests';
 import React from 'react';
-import { expect } from '@jest/globals';
-import { render } from '@testing-library/react';
-// import { UnknownRoute } from '../components/index';
+// import { expect } from '@jest/globals';
+import { render, screen  } from '@testing-library/react';
+import { UnknownRoute } from '../components/index';
 
 it("Dummy unit test", () => {
     const actual = 1 + 1;
@@ -20,7 +20,8 @@ it("Dummy unit test 2", () => {
 });
 
 it("Dummy unit test 3", () => {
-    render(<button>Click Me</button>)
+    render(<button>Click Me</button>);
+    expect(screen.getByText('Click Me')).toBeInTheDocument();
 });
 
 // it("Dummy unit test 4", () => {
--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,15 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jest-environment-jsdom',
     globals: {
+        // we must specify a custom tsconfig for tests because we need the typescript transform
+        // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+        // can see this setting in tsconfig.jest.json -> "jsx": "react"
         "ts-jest": {
-            tsconfig: "tsconfig.test.json",
+            tsconfig: "tsconfig.jest.json",
             diagnostics: {
                 warnOnly: true
             }
         }
-    }
+    },
+    setupFilesAfterEnv: ['./jest.setup.ts'],
 };
