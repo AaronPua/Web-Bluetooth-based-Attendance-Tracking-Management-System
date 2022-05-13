b197078581bfd5ef560aa353253cd37c7d7e313f
Aaron Pua <aaronpua@hotmail.com>
2022-05-12 22:37:35 -0700

    Some changes for E2E tests for the final report.

--- tests/cypress/integration/auth/02-resend-verification.test.ts
+++ tests/cypress/integration/auth/02-resend-verification.test.ts
@@ -3,9 +3,19 @@ describe('resend verification email', function() {
         cy.visit("/verify-email");
     });
 
-    it('user requests resend verification email', () => {
+    it('fail - user requests resend verification email', () => {
         cy.contains('Click Here').click();
 
+        cy.get('input[name="email"]').type('not_exist@fake.com');
+        cy.get('button[type="submit"]').contains('Resend').click();
+
+        cy.contains('User not found according to this email address.');
+    });
+
+    it('success - user requests resend verification email', () => {
+        cy.contains('Click Here').click();
+
+        cy.get('input[name="email"]').clear();
         cy.get('input[name="email"]').type('test_user@fake.com');
         cy.get('button[type="submit"]').contains('Resend').click();
 
